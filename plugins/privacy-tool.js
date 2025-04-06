8f97b2b4a27a9f9b638cb1c2ea98d5e5e05dfec9
Dark hacker 😈 <javieldev@gmail.com>
2025-04-06 17:32:04 +0300

    Update privacy-tool.js

--- plugins/privacy-tool.js
+++ plugins/privacy-tool.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/nk71o3.jpg` }, // Replace with privacy-themed image if available
+                image: { url: `https://files.catbox.moe/efiab6.jpg` }, // Replace with privacy-themed image if available
                 caption: privacyMenu,
                 contextInfo: {
                     mentionedJid: [m.sender],
